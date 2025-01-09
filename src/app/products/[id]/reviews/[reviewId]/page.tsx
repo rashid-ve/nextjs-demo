9e68c32f8d88909372b13244d5b433410b1e8198
rashid-ve <rashidashraf@teckvalley.com>
2025-01-09 17:10:49 +0530

    Error handling

--- src/app/products/[id]/reviews/[reviewId]/page.tsx
+++ src/app/products/[id]/reviews/[reviewId]/page.tsx
@@ -19,7 +19,15 @@ export const generateMetadata = async ({
   };
 };
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ReviewPage({ params }: Props) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review!");
+  }
   const { id, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
     // notFound();
