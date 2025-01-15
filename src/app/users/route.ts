5bfe9dcef51364f50d41e1c0bbdcd1005e29b89a
rashid-ve <rashidashraf@teckvalley.com>
2025-01-15 20:09:09 +0530

    Query params and Cookies setup

--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -1,10 +1,22 @@
+import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
+
 export const users = [
   { id: 1, name: "John Doe" },
   { id: 2, name: "Jane Doe" },
 ];
 
-export async function GET() {
-  return Response.json(users);
+export async function GET(request: NextRequest) {
+  const cookieStore = await cookies();
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredUsers = query
+    ? users.filter((users) => users.name.includes(query))
+    : users;
+  cookieStore.set("resultsPerPage", "10");
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+  return Response.json(filteredUsers);
 }
 
 export async function POST(request: Request) {
@@ -17,6 +29,7 @@ export async function POST(request: Request) {
   return new Response(JSON.stringify(newUser), {
     headers: {
       "Content-Type": "application/json",
+      "Set-Cookie": "theme=dark",
     },
     status: 201,
   });
